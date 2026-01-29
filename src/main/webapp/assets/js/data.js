5527e1a051d9bb4dee9f54226fdb61ef39840c3f
kkumar90 <kumarkundan0290@gmail.com>
2026-01-29 20:11:27 +0530

    Update data.js

--- src/main/webapp/assets/js/data.js
+++ src/main/webapp/assets/js/data.js
@@ -16,7 +16,7 @@ let movies = [
   },
   {
     name: "loki",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    des: "Lorem ipsum dolor sit amet consectetur adipising elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
     image: "assets/img/slider 1.png",
   },
   {
